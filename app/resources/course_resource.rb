5f946b6bd524d7b948465fe80a56686d7b0d05d2
anonymous anonymous <rahul.jayaprakash@kellogg.northwestern.edu>
2021-12-09 06:08:03 +0000

    Generate graphiti resources for courses

---
+++ app/resources/course_resource.rb
@@ -0,0 +1,13 @@
+class CourseResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :course_number, :string
+  attribute :course_name, :string
+  attribute :course_description, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
---
+++ spec/api/v1/courses/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "courses#create", type: :request do
+  subject(:make_request) do
+    jsonapi_post "/api/v1/courses", payload
+  end
+
+  describe 'basic create' do
+    let(:params) do
+      {
+        # ... your attrs here
+      }
+    end
+    let(:payload) do
+      {
+        data: {
+          type: 'courses',
+          attributes: params
+        }
+      }
+    end
+
+    it 'works' do
+      expect(CourseResource).to receive(:build).and_call_original
+      expect {
+        make_request
+        expect(response.status).to eq(201), response.body
+      }.to change { Course.count }.by(1)
+    end
+  end
+end
