4c5ea4248440a52f8a901fc892b3d4c00e3abec0
anonymous anonymous <rahul.jayaprakash@kellogg.northwestern.edu>
2021-12-09 06:06:52 +0000

    Whitelist columns in CourseRating for ActiveAdmin

--- app/admin/course_ratings.rb
+++ app/admin/course_ratings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CourseRating do
 
+ permit_params :user_id, :facet_1_rating, :facet_2_rating, :facet_3_rating, :facet_4_rating, :facet_5_rating, :descriptive_q1, :descriptive_q2, :descriptive_q3, :course_offering_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
